dc35ea114dc1246b936e533df4f802564fe27e1c
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2022-11-15 17:48:45 -0800

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router , Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </Router>
     </div>
